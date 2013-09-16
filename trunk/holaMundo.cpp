da30a7a1a0f18da4a717cce86b0830f44ccc8d2d
ezequielrozen@gmail.com <ezequielrozen@gmail.com@4f68a5b4-7bad-891a-6c68-f6bc41bc5668>
2013-09-16 00:47:32 +0000

    Probando eclipse

---
+++ trunk/holaMundo.cpp
@@ -0,0 +1,17 @@
+/*
+ * holaMundo.cpp
+ *
+ *  Created on: 15/09/2013
+ *      Author: eze
+ */
+
+#include <iostream>
+using namespace std;
+
+int main(){
+	cout << "Hola Mundo" << endl;
+	return 0;
+}
+
+
+
