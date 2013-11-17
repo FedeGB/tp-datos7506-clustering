dbec7d984fef950a0ab6875ce6d5e26966ca33a3
fedeagb@gmail.com <fedeagb@gmail.com@4f68a5b4-7bad-891a-6c68-f6bc41bc5668>
2013-11-17 21:51:39 +0000

    Me falto agregar los documentos nuevos :P

---
+++ Document.h
@@ -0,0 +1,22 @@
+#ifndef DOCUMENT_H
+#define DOCUMENT_H
+
+#include <string>
+
+class Document {
+	private:
+		bool clusteroid;
+	
+	public:
+		std::string name;
+		unsigned int number;
+		
+		Document(const std::string nombre, unsigned int numero);
+		~Document();
+		bool isCluteroid();
+		void makeCluseroid(); // pasa a ser un clusteroide
+		void notClusteroid(); // deja de ser clusteroide
+	
+};
+
+#endif
