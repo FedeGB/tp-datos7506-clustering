51bd5cbb8c6dc282c25749f61d318bafa9eb97b3
ivi.rhcp <ivi.rhcp@4f68a5b4-7bad-891a-6c68-f6bc41bc5668>
2013-11-28 15:20:39 +0000

    Arreglado persistor

--- trunk/Persistor.cpp
+++ trunk/Persistor.cpp
@@ -12,21 +12,22 @@ Persistor::~Persistor() {
 
 void Persistor::saveClusters(const std::vector<std::vector<uint64_t>* >& hashDocs, const std::vector<Cluster*>& clusters) {
 
-	std::ofstream fClusteroids("saves/clusteroides.txt", std::ofstream::binary);
+	std::ofstream fClusteroids("saves/clusteroides.clus", std::ios::out | std::ofstream::binary | std::ios::trunc);
 	int count = 1;
 	for (std::vector<Cluster*>::const_iterator it = clusters.begin(); it != clusters.end(); ++it) {
 		std::stringstream nombre;
 		nombre << "saves/clusters/cluster";
 		nombre << count; 
 		nombre << ".txt";	
-		std::ofstream fCluster(nombre.str().c_str());
+		std::ofstream fCluster(nombre.str().c_str(),std::ios::out | std::ios::trunc);
 		count++;
 		//Persisto el clusteroide que pertenece a un cluster con todos sus valores de minhash
 		unsigned int nroClusteroide = ((*it)->getClusteroide())->number;
 		std::vector<uint64_t>* hashClusteroide = hashDocs[nroClusteroide];
 		for (std::vector<uint64_t>::iterator hashes = hashClusteroide->begin(); hashes != hashClusteroide->end(); ++hashes){
 			//std::cout << (*hashes) << std::endl;
-			fClusteroids << (*hashes);
+			//fClusteroids << (*hashes);
+			fClusteroids.write((char*)&(*hashes),8);
 		}
 		//Guardo los nombres de los documentos que pertenecen a un cluster
 		(*it)->iniciarDocumentos();
@@ -40,14 +41,15 @@ void Persistor::saveClusters(const std::vector<std::vector<uint64_t>* >& hashDoc
 }
 
 void Persistor::cargarClusteroides(std::vector<std::vector<uint64_t>* >& hashDocs){
-	std::ifstream fClusteroids("saves/clusteroides.txt", std::ifstream::binary);
+	std::ifstream fClusteroids("saves/clusteroides.clus", std::ios::in | std::ifstream::binary);
 	unsigned int clusteroide=1;
 	while (fClusteroids.good()) {
-		std::vector<uint64_t>* hashes = new std::vector<uint64_t>(240);
+		std::vector<uint64_t>* hashes = new std::vector<uint64_t>;
 		unsigned int i=0;
 		while (fClusteroids.good() && i<240) {
 			uint64_t hash;
-			fClusteroids >> hash;
+			fClusteroids.read((char*)&hash,8);
+			//fClusteroids >> hash;
 			std::cout << "se guardo " << "clusteroide "<< clusteroide <<" hash: " << i << " " << hash << std::endl;
 			hashes->push_back(hash);
 			i++;
