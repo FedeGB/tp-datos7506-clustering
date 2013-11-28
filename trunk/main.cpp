c9d706d609089141d53da312607b8789fb102ffe
fedeagb@gmail.com <fedeagb@gmail.com@4f68a5b4-7bad-891a-6c68-f6bc41bc5668>
2013-11-28 16:51:50 +0000

    El main real! Deberia fucnionar todo este implementado o no. Al indexador le agregue q le ponga el / al final del path xq se supone q el path nos lo pasan sin eso al final. Igualmente si se agrega uno de mas y queda doble no se rompio, asi q todo bien :P

--- trunk/main.cpp
+++ trunk/main.cpp
@@ -8,31 +8,124 @@
 #include "GeneradorCluster.h"
 #include "Persistor.h"
 #include <iostream>
+#include <string.h>
+#include <string>
+#include <unistd.h>
+// http://www.gnu.org/software/libc/manual/html_node/Getopt.html unistd.h 'man'
+
+#define ARGERROR -1
 
 using namespace std;
 
-int main() {
+int main(int argc, char** argv) {
 	
-	Indexador indexar("./docs/");
+	if( argc < 2 ) {
+		fprintf(stderr, "La cantidad de argumentos es insuficiente\n");
+		return ARGERROR;
+	}
+	
+	int opt, dflag, oflag, k;
+	dflag = oflag = k = 0;
+	std::string path;
+	bool multiple = false;
+	
+	while( (opt = getopt(argc, argv, "a:c::d:glo:")) != -1 ) {
+		
+		switch(opt) {
+			case 'a':
+				if(argc > 3) {
+					fprintf(stderr, "Hay parametros de más\n");
+					return ARGERROR;
+				}
+				
+				return 0;
+			
+			case 'c':
+				k = (unsigned)atoi(argv[optind]);
+				break;
+			
+			case 'd':
+				path = std::string(optarg);
+				dflag = 1;
+				break;
+			
+			case 'g':
+				if(argc > 2) {
+					fprintf(stderr, "Hay parametros de más\n");
+					return ARGERROR;
+				}
+				
+				return 0;
+			
+			case 'l':
+				if(argc > 2) {
+					fprintf(stderr, "Hay parametros de más\n");
+					return ARGERROR;
+				}
+				
+				return 0;
+			
+			case 'o':
+				if(strcmp(optarg, "Y") == 0) {
+					multiple = true;
+				}
+				if(strcmp(optarg, "N") == 0) {
+					multiple = false;
+				}
+				else {
+					fprintf(stderr, "El argumento del parametro -o es incorrecto\n");
+					return ARGERROR;
+				}
+				oflag = 1;
+				break;
+				
+			case '?':
+				if( optopt == 'd' ) {
+					fprintf(stderr, "Falto el path a los documentos\n");
+				}
+				if( optopt == 'o' ) {
+					fprintf(stderr, "Falto indicar el argumento de -o\n");
+				}
+				if( optopt == 'a' ) {
+					fprintf(stderr, "Falto el path al documento a agregar\n");
+				}
+				else {
+					fprintf(stderr, "Parametro no reconocido\n");
+				}
+				return ARGERROR;
+			default:
+				return ARGERROR;
+			
+		}
+	}
+	
+	// Llegamos aca si es un repositorio nuevo y hacemos el cluster de 0
+	// En los demas casos se resuelve dentro del case correspondiente
+	if(!dflag && !oflag) {
+		fprintf(stderr, "Falto algun parametro\n");
+		return ARGERROR;
+	}
+	
+	Indexador indexar(path);
 	Document* doc_actual;
 	Parser* parser;
 	minHash hashmin(7);
 	vector<vector<uint64_t>* > hashDocs;
-	vector<vector<uint64_t>* > hashDocs2;
 	vector<Document*> docs;
 	Cluster* cluster;
 	unsigned cantDocs = 0;
+	
+	cout << "Indexando arhchivos..." << endl;
 	while(indexar.quedanArchivos()) {
 		doc_actual = indexar.obtenerDocumento();
 		parser = new Parser(indexar.Path+doc_actual->name,7);
-		vector<uint64_t>* hashings = new vector<uint64_t>(240,18446744073709551615U);
+		vector<uint64_t>* hashings = new vector<uint64_t>(240);
 		while (parser->tieneShingle()) {
 			hashmin.doMinHash(parser->obtenerShingle(),*hashings);
 		}
 		hashDocs.push_back(hashings);
 		docs.push_back(doc_actual);
 		delete parser;
-		//delete doc_actual;
 		cantDocs++;
 	}
 
@@ -40,31 +133,19 @@ int main() {
 	lsHashing.doLsh();
 	
 	vector<Cluster*> conjunto;
-	
 	GeneradorCluster generador;
-	//generador.obtenerClusters(3, true, docs, conjunto, lsHashing);
-	generador.KMeans(cantDocs, false, docs, conjunto, lsHashing);
-	// for (int i = 0; i < cantDocs; i++){
-	// 	for (int j = 0; j < cantDocs; j++){
-	// 		cout<<"Distancia "<<i<<" "<<j<<": "<<lsHashing.distancia(i,j)<<endl;
-			
-	// 	}
-	// }
+	
+	if(k != 0 ) {
+		generador.obtenerClusters(k, multiple, docs, conjunto, lsHashing);
+	}
+	else {
+		generador.KMeans(cantDocs, multiple, docs, conjunto, lsHashing);
+	}
 
 	Persistor persistor;
 	persistor.saveClusters(hashDocs,conjunto);
-	Persistor otroPersistor;
-	otroPersistor.cargarClusteroides(hashDocs2);
-	/*for (vector<vector<uint64_t>* >::iterator it1 = hashDocs2.begin(); it1 != hashDocs2.end(); ++it1){
-		for (vector<uint64_t>::iterator it2 = (*it1)->begin(); it2 != (*it1)->end(); ++it2){
-			std::cout << *it2 << std::endl;
-		}
-	}*/
-
-
-	for (vector<vector<uint64_t>*>::iterator it = hashDocs2.begin(); it != hashDocs2.end(); ++it){
-		delete (*it);
-	}
+	
+	// Liberamos memoria
 	int i = 0;
 	for (vector<vector<uint64_t>*>::iterator it = hashDocs.begin(); it != hashDocs.end(); ++it){
 		delete docs[i];
@@ -74,5 +155,6 @@ int main() {
 	for(vector<Cluster*>::iterator ite = conjunto.begin(); ite != conjunto.end(); ++ite) {
 		delete (*ite);
 	}
+	
 	return 0;
 }
